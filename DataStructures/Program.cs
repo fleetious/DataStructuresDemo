49bf3be1e0a9c39648f67543dab1dc295de7462c
Enzo.Liu <Enzo.Liu@GMR-118-2-2>
2025-02-09 12:00:08 -0800

    Add project files.

---
+++ DataStructures/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * All of the data structures encountered later on will be implemented with generics. As you will see, this approach is especially useful with collection classes.
+ * All of the data structures encountered later on will be implemented with generics. As you will see, this approach is especially useful with collection classes.
+ * All of the data structures encountered later on will be implemented with generics. As you will see, this approach is especially useful with collection classes.
+ * All of the data structures encountered later on will be implemented with generics. As you will see, this approach is especially useful with collection classes.
+*/
+using System.Collections.Generic;
+
+namespace DataStructures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Collections.Generic.List<int> testList = new()
+        }
+    }
+}

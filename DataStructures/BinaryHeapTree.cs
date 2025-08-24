2b08fbafe70d594e4698438039f2d9970014f6f4
enzo.liu <enzo.liu@GMR-118-1-3>
2025-08-24 11:59:43 -0700

    heap start

---
+++ DataStructures/BinaryHeapTree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    internal class BinaryHeapTree<T> where T : IComparable<T>
+    {
+        private T[] values;
+        private int 
+
+        public BinaryHeapTree(T[] values)
+        {
+            this.values = values;
+        }
+
+        public void Insert(T value)
+        {
+            if(values == null)
+            {
+                values = new T[1];
+                values[0] = value;
+                return;
+            }
+
+            if()
+        }
+    }
+}

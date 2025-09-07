89bdae9df9d26dd21740aee2252de297f0cfe37e
enzo.liu <enzo.liu@GMR-118-1-3>
2025-09-07 11:59:19 -0700

    BHT Insert (function & test)

---
+++ DataStructures.Tests/BinaryHeapTreeTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DataStructures;
+
+namespace DataStructures.Tests
+{
+    public class BinaryHeapTreeTest
+    {
+        [Theory]
+        [InlineData(new int[] { 0 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 })]
+        public void Insert(int[] values)
+        {
+            BinaryHeapTree<int> tree = new BinaryHeapTree<int>();
+
+            for(int i = 0; i < values.Length; i++)
+            {
+                tree.Insert(values[i]);
+            }
+        }
+    }
+}
--- DataStructures/BinaryHeapTree.cs
+++ DataStructures/BinaryHeapTree.cs
@@ -6,14 +6,15 @@ using System.Threading.Tasks;
 
 namespace DataStructures
 {
-    internal class BinaryHeapTree<T> where T : IComparable<T>
+    public class BinaryHeapTree<T> where T : IComparable<T>
     {
         private T[] values;
-        private int 
+        private int count = 0;
+        private int level = 0;
 
-        public BinaryHeapTree(T[] values)
+        public BinaryHeapTree()
         {
-            this.values = values;
+            values = new T[1];
         }
 
         public void Insert(T value)
@@ -25,7 +26,58 @@ namespace DataStructures
                 return;
             }
 
-            if()
+            if (count >= values.Length) ResizeArrayUp();
+
+            values[count] = value;
+
+            HeapifyUp(count);
+
+            count++;
+        }
+
+        private void HeapifyUp(int index)
+        {
+            if (values == null) return;
+            if(index == 0) return;
+
+            T value = values[index];
+            int parentIndex = GetParentIndex(index);
+
+            if (value.CompareTo(values[parentIndex]) > 0)
+            {
+                Swap(ref values[index], ref values[parentIndex]);
+            }
+
+            HeapifyUp(parentIndex);
+        }
+
+        private int GetParentIndex(int childIndex)
+        {
+            int childDelta = values.Length - childIndex; // delta to top of level
+            int parentDelta = (int)Math.Floor((double)childDelta / 2); // delta to top of level
+            int childLevelBottom = values.Length - (int)Math.Pow(2, level);
+            
+            return childLevelBottom - parentDelta;
+        }
+
+        private void Swap(ref T item1, ref T item2) // stole from wiki
+        {
+            T temp = item1;
+            item1 = item2;
+            item2 = temp;
+        }
+
+        private void ResizeArrayUp()
+        {
+            level++;
+            T[] temp = new T[values.Length + (int)Math.Pow(2, level)];
+
+            for(int i = 0; i < values.Length; i++)
+            {
+                temp[i] = values[i];
+            }
+
+            values = temp;
         }
     }
 }

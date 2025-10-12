296e59901efce451905f0cecee7da57d0512fc7f
GMR Student <gmrstudent@outlook.com>
2025-10-12 12:03:15 -0700

    hbvhhgghfhfgfghghfghfghfghfghfhgfhgfhgfhgfgfhgf lamaoo

--- DataStructures.Tests/AVLTreeTest.cs
+++ DataStructures.Tests/AVLTreeTest.cs
@@ -18,6 +18,35 @@ namespace DataStructures.Tests
             {
                 tree.Insert(valuesToInsert[i]);
             }
+
+            for (int i = 0; i < valuesToInsert.Length; i++)
+            {
+                Assert.True(tree.Contains(valuesToInsert[i]));
+            }
+        }
+
+        [Theory]
+        [InlineData(new int[] { 5, 7, 8, 4 }, new int[] { 7 })]
+        public void Remove(int[] valuesToInsert, int[] valuesToRemove) // chatgpt my beeloved
+        { // woww this is literally the same as the generic tree test lmao i should make a base class for these tests later :D :D :D :D :D :D :D :D :D :D :D yes :D :D :D :D :D :D :D  :D :D :D
+            AVLTree<int> tree = new AVLTree<int>();
+            for (int i = 0; i < valuesToInsert.Length; i++)
+            {
+                tree.Insert(valuesToInsert[i]);
+            }
+            for (int i = 0; i < valuesToRemove.Length; i++)
+            {
+                tree.Remove(valuesToRemove[i]);
+            }
+            for (int i = 0; i < valuesToRemove.Length; i++)
+            {
+                Assert.True(!tree.Contains(valuesToRemove[i]));
+            }
+            for (int i = 0; i < valuesToInsert.Length; i++)
+            {
+                if (!valuesToRemove.Contains(valuesToInsert[i]))
+                    Assert.True(tree.Contains(valuesToInsert[i]));
+            }
         }
     }
 }
--- DataStructures/AVLTree.cs
+++ DataStructures/AVLTree.cs
@@ -30,10 +30,10 @@ namespace DataStructures
 
     public class AVLTree<T> where T : IComparable<T>
     {
-        public class Node // rename to BinarySearchTreeLeaf or smth after making tree
+        public class Node<T> // rename to BinarySearchTreeLeaf or smth after making tree
         {
-            public Node Left;
-            public Node Right;
+            public Node<T> Left;
+            public Node<T> Right;
 
             public int Height;
             public int Balance;
@@ -41,7 +41,7 @@ namespace DataStructures
             public T Value;
 
             public Node(T value) : this(value, null, null) { }
-            public Node(T value, Node left, Node right)
+            public Node(T value, Node<T> left, Node<T> right)
             {
                 Value = value;
                 Left = left;
@@ -49,66 +49,119 @@ namespace DataStructures
             }
         }
 
-        public Node Root;
+        public Node<T> Root;
 
         public AVLTree() : this(null) { }
 
-        public AVLTree(Node root)
+        public AVLTree(Node<T> root)
         {
             Root = root;
         }
 
         public void Insert(T value)
         {
-            if (Root == null)
-            {
-                Root = new Node(value);
-                return;
-            }
-
-            RecursiveInsert(value, Root);
+            Root = RecursiveInsert(value, Root);
         }
 
-        // yoo what if we just use search to find the node and then in remove we handle the childrenn
-        public Node Remove(T value)
+        // yoo what if we just use search to find the node and then in remove we handle the childrenn nuh uh
+        public Node<T> Remove(T value)
         {
-            throw new NotImplementedException();
+            Node<T> node = RecursiveSearch(value, Root);
+            Node<T> parent = node; if (node == null) throw new InvalidOperationException("Value not found in tree"); // ily chatgpt
+
+            Node<T> leftChild = node.Left; Node<T> rightChild = node.Right;
+
+
+            node = null;
+            if (leftChild != null)
+                Insert(leftChild.Value); if (rightChild != null)
+            {
+                Insert(rightChild.Value);
+
+
+            }
+
+            return parent;
         }
 
         public bool Contains(T value) => Search(value) != null;
 
-        public Node Search(T value) => RecursiveSearch(value, Root);
+        public Node<T> Search(T value) => RecursiveSearch(value, Root);
 
-        private Node RecursiveInsert(T value, Node currentNode)
+        private static Node<T> RecursiveInsert(T value, Node<T> currentNode)
         {
-            if (currentNode.Value.CompareTo(value) < 0 && currentNode.Right != null)
+            if (currentNode == null)
             {
-                return RecursiveInsert(value, currentNode.Right);
+                return new Node<T>(value);
             }
-            else if (currentNode.Left != null)
-            {
-                return RecursiveInsert(value, currentNode.Left);
-            }
-
             if (currentNode.Value.CompareTo(value) < 0)
             {
-                currentNode.Right = new Node(value);
+                Node<T> temp = RecursiveInsert(value, currentNode.Right);
+                currentNode.Right = temp;
             }
             else
             {
-                currentNode.Left = new Node(value);
+                Node<T> temp = RecursiveInsert(value, currentNode.Left);
+                currentNode.Left = temp;
             }
-
+            //add rotations here
             return currentNode;
         }
 
-        // think of this similar to insert, go down tree and find the node ykyk
-        private Node RecursiveSearch(T value, Node currentNode)
+        private static Node<T> RecursiveRemove(T value, Node<T> currentNode)
         {
-            throw new NotImplementedException();
-        }
+            if (currentNode == null) return null;
 
-        private T[] RecursiveTraversal() // params are TBD
+            if (currentNode.Value.CompareTo(value) == 0)
+            {
+                currentNode = Remove(currentNode);
+                return currentNode;
+            }
+            else if (currentNode.Value.CompareTo(value) < 0)
+            {
+                currentNode.Right = RecursiveRemove(value, currentNode.Right);
+            }
+            else
+            {
+                currentNode.Left = RecursiveRemove(value, currentNode.Left);
+            }
+
+            return currentNode;
+
+            // this is wrong lmao it made much more sense in my head at 2 am lmao actually i think it was just incomplete but idk how to finish it lmao lol
+            //if (currentNode == null) return null;
+
+            //if (currentNode.Value.CompareTo(value) == 0) return currentNode = null;
+            //if (currentNode.Value.CompareTo(value) < 0)
+            //{
+            //    return RecursiveSearch(value, currentNode.Right);
+            //}
+            //else
+            //{
+            //    return RecursiveSearch(value, currentNode.Left);
+            //}
+        }
+        static Node<T> Remove(Node<T> node)
+        {
+            
+        }
+        // think of this similar to insert, go down tree and find the node ykyk
+        private Node<T> RecursiveSearch(T value, Node<T> currentNode)
+        {
+            if (currentNode == null) return null;
+
+            if (currentNode.Value.CompareTo(value) == 0) return currentNode;
+            if (currentNode.Value.CompareTo(value) < 0)
+            {
+                return RecursiveSearch(value, currentNode.Right);
+            }
+            else
+            {
+                return RecursiveSearch(value, currentNode.Left);
+            }
+        }
+        // tex said u dont have to do level order traversal so imma not do it !!
+        private T[] RecursiveTraversal(Node<T> node) // params are TBD
         {
             throw new NotImplementedException();
         }

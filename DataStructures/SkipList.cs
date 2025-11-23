12f3e038b9e5d88c0143383ef0b8b5c9528cd265
enzo.liu <enzo.liu@GMR-118-1-3>
2025-11-23 11:59:13 -0800

    started skip list

--- DataStructures/SkipList.cs
+++ DataStructures/SkipList.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace DataStructures
 {
-    internal class SkipList
+    internal class SkipList<T> where T : IComparable<T>
     {
+        public class Node<T> where T : IComparable<T>
+        {
+            public T Value; // Value of the node
+            public Node<T> Next; // Rightward connection
+            public Node<T> Down; // Downward connection
+            public int Height { get; } // Vertical height of the node
+
+            public Node(T value)
+            {
+                Value = value;
+                
+                Random rand = new Random();
+                Node<T> currentDown = this;
+                while(rand.Next(2) == 1)
+                {
+                    currentDown = new Node<T>(value, currentDown);
+                    Down = currentDown;
+                    Height++;
+                }
+            }
+            public Node(T value, Node<T> down)
+            {
+                Value = value;
+
+                Down = down;
+            }
+        }
     }
 }
